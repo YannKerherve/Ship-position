49eab02bf7199989b365d7f2ac0a32a7f4fc9e9a
Yann Kerherve <ya.kerherve@gmail.com>
2026-02-14 15:56:25 +0100

    Update pluginConfig.ts

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -1,13 +1,13 @@
 import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
-    name: 'windy-plugin-my-plugin',
-    version: '0.1.0',
-    icon: '🔌',
+    name: 'windy-plugin-shipposition',
+    version: '3.0.0',
+    icon: '🛰️',
     title: 'Ship Position',
     description: 'Ship Position',
     author: 'Yann Kerherve (ENSM)',
-    repository: 'https://github.com/windycom/windy-plugin-template',
+    repository: 'https://github.com/YannKerherve/Ship-position',
     desktopUI: 'embedded',
     mobileUI: 'small',
     routerPath: '/my-plugin',

9630deeec1477bb6486bd95b1d91b5ac7436bf7d
Yann Kerherve <ya.kerherve@gmail.com>
2026-06-01 17:23:24 +0200

    Bump version to 3.0.4 in pluginConfig

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-shipposition',
-    version: '3.0.3',
+    version: '3.0.4',
     icon: '🛰️',
     title: 'Ship Position',
     description: 'Ship Position',
